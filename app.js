5f38b61dfc9324cd633e4872865cd14c788af4ff
Hitesh Sahu <hd-hitesh@users.noreply.github.com>
2020-07-29 21:26:47 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+$('.js-form').submit(function(event){
+	event.preventDefault();
+	var UserTextElement= $(event.currentTarget).find('#message');
+	//var Name=$(event.currentTarget).find('#name');
+	$(".js-display").prepend(UserTextElement.val()+"<br>");
+
+	UserTextElement.val("");
+	//Name.val("");
+	$(".js-display").removeClass("hidden");
+});
+
+
+$(document).ready(function () {
+     $('a[href^="#"]').on('click', function (e) {
+         e.preventDefault();
+
+         var target = this.hash,
+             $target = $(target);
+
+         $('html, body').stop().animate({
+             'scrollTop': $target.offset().top 
+         }, 700, 'swing', function () {
+             window.location.hash = target;
+         });
+     });
+ });
+
+
+
